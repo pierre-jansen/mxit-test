2eab4f71bdd807321b1eb80a3802e8159dad4bd3
Pierre Jansen <pierre.jansen@wonga.com>
2013-11-06 13:10:45 +0200

    initial

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <h1>Hello test!</h1>
+  </body>
+</html>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
